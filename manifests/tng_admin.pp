5d6c7ecf247ff398f150035d0252156cdf3af3a2
Paolo Cadiz <pcadiz@cambridge.org>
2017-01-11 10:02:05 +0800

    added vhost

--- manifests/tng_admin.pp
+++ manifests/tng_admin.pp
@@ -5,4 +5,16 @@ class profile::tng_admin {
     include php
     include mysql::client
 
+    apache::vhost {'admin.dev.test.cambridge.org':
+    	port        => '80',
+    	docroot     => '/mnt/data/www/admin.cambridge.edu.au',
+    	directories => [{
+    		path           => '/mnt/data/www/admin.cambridge.edu.au',
+    		options        => ['Indexes','FollowSymLinks'],
+    		allow_override => ['All'],
+    		order          => 'Allow,Deny',
+    		allow          => 'from all',
+    	}],
+	}
+
 }
