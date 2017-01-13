4a989b269f8caa47bd6085b026ef9f882a70d466
Paolo Cadiz <pcadiz@cambridge.org>
2017-01-13 09:41:30 +0800

    updated apache call

--- manifests/tng/apache.pp
+++ manifests/tng/apache.pp
@@ -1,6 +1,19 @@
 class profile::tng::apache {
 	
-	include apache
-	include apache::mod::php
+	include ::apache::mod::php
+
+	file {'/mnt':
+		ensure => directory,
+	}
+
+	file {'/mnt/data':
+		ensure  => directory,
+		require => File['/mnt'],
+	}
+
+	class {'::apache':
+		docroot => '/mnt/data/www',
+		require => File['/mnt/data'],
+	}
 
 }
