ca94d81f5affbaf9e54a1a9c829a61b53cae4969
Paolo Cadiz <pcadiz@cambridge.org>
2017-01-13 09:44:30 +0800

    tabulation, removed apache::mod::php for now

--- manifests/tng/apache.pp
+++ manifests/tng/apache.pp
@@ -1,19 +1,17 @@
 class profile::tng::apache {
-	
-	include ::apache::mod::php
 
-	file {'/mnt':
-		ensure => directory,
-	}
+    file {'/mnt':
+        ensure => directory,
+    }
 
-	file {'/mnt/data':
-		ensure  => directory,
-		require => File['/mnt'],
-	}
+    file {'/mnt/data':
+        ensure  => directory,
+        require => File['/mnt'],
+    }
 
-	class {'::apache':
-		docroot => '/mnt/data/www',
-		require => File['/mnt/data'],
-	}
+    class {'::apache':
+        docroot => '/mnt/data/www',
+        require => File['/mnt/data'],
+    }
 
 }
