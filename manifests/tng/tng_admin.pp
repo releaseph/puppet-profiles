cb7741bf2f4ffe04f719b4473ae697e76c717afe
Paolo Cadiz <pcadiz@cambridge.org>
2017-01-16 09:42:27 +0800

    added url variable to tng_admin profile

--- manifests/tng/tng_admin.pp
+++ manifests/tng/tng_admin.pp
@@ -1,20 +1,17 @@
-class profile::tng::tng_admin {
+class profile::tng::tng_admin(
+    $url = undef,
+) {
 
-    include apache
-    include apache::mod::php
-    include php
-    include mysql::client
-
-    apache::vhost {'admin.dev.test.cambridge.org':
-    	port        => '80',
-    	docroot     => '/mnt/data/www/admin.cambridge.edu.au',
-    	directories => [{
-    		path           => '/mnt/data/www/admin.cambridge.edu.au',
-    		options        => ['Indexes','FollowSymLinks'],
-    		allow_override => ['All'],
-    		order          => 'Allow,Deny',
-    		allow          => 'from all',
-    	}],
-	}
+    apache::vhost {"$url":
+        port        => '80',
+        docroot     => '/mnt/data/www/admin.cambridge.edu.au',
+        directories => [{
+            path           => '/mnt/data/www/admin.cambridge.edu.au',
+            options        => ['Indexes','FollowSymLinks'],
+            allow_override => ['All'],
+            order          => 'Allow,Deny',
+            allow          => 'from all',
+        }],
+    }
 
 }
