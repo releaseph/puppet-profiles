f76e14d2c5dd3e6b1e87c97c148dc98fc4ca9175
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-03 10:41:48 +0800

    added r10k profile

---
+++ manifests/devops/r10k.pp
@@ -0,0 +1,19 @@
+class profile::devops::r10k (
+    $control_repo = undef,
+){
+    class { 'r10k':
+        remote => $control_repo,
+    }
+
+    class { 'r10k::webhook::config':
+        use_mcollective => false,
+        enable_ssl      => false,
+    }
+
+    class { 'r10k::webhook':
+        user            => 'root',
+        group           => '0',
+        use_mcollective => false,
+        require         => Class['r10k::webhook::config'],
+    }
+}
