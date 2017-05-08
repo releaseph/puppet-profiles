1ed4ac75307e4e456fc4de6b065a106e6096223b
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-08 14:48:32 +0800

    locked sinatra to a specific version

--- manifests/devops/r10k.pp
+++ manifests/devops/r10k.pp
@@ -5,6 +5,15 @@ class profile::devops::r10k (
         remote => $control_repo,
     }
 
+    ### TODO - Ensure that the puppet-provided ruby is at least 2.2 before removing this package call
+    # The latest version of Sinatra requires Ruby 2.2, but Puppet-provided Ruby is only 2.1
+    # System Ruby can't be used without updating the webhook (since webhook relies on puppet_gem starting puppet 4.10.0)
+    package { 'sinatra':
+        ensure   => '1.4.8',
+        provider => 'puppet_gem',
+    } ->
+    ### End of TODO
+
     class { 'r10k::webhook::config':
         use_mcollective => false,
         enable_ssl      => false,
