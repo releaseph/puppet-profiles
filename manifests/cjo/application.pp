a10833ae686e77e715009755536a7033fb71154e
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-10 14:55:34 +0800

    removed user/group and properties file

--- manifests/cjo/application.pp
+++ manifests/cjo/application.pp
@@ -3,21 +3,6 @@
 
 class profile::cjo::application {
 
-    group { 'cjogroup':
-        ensure => present,
-    }
-
-    -> user { 'cjo':
-        ensure => present,
-        groups => [ 'cjogroup' ],
-    }
-
-    file { '/app/jboss-5.1.0.GA/conf/SystemApplicationResources.properties':
-        ensure => file,
-        group  => 'cjogroup',
-        owner  => 'cjo',
-    }
-
     puppet_config::java_property { 'uhy_bib_supplementery_id':
         file  => '/app/jboss-5.1.0.GA/conf/SystemApplicationResources.properties',
         key   => 'uhy_bib_supplementary_id',
